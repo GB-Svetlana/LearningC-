d6ed94f2c0d63bea3df5b9002082b240064ec9b4
Svetlana <levanova_si@mail.ru>
2023-02-11 20:50:10 +0300

    Add Task 29

--- Homework4/Task29/Program.cs
+++ Homework4/Task29/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+System.Console.WriteLine("Введите длину массива: ");
+int lenArray = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,20);
+    Console.Write(randomArray[i] + " ");
+}
